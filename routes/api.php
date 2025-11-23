a28ec573710834e4c51645b06b19d795e2b1ae61
salman <khattaksd@gmail.com>
2025-11-23 16:19:00 -0500

    wip

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,14 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Stringable;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::get('/quote', function (Request $request) {
-        [$text, $author] = (new Stringable(Inspiring::quotes()->random())->explode('-'));
+        $quote = Inspiring::quotes()->random();
+        [$text, $author] = str($quote)->explode('-');
         return [
             'text' => trim($text),
             'author' => trim($author),
